396820e453dae3f335ec3566ce3f843273e1f194
chodolak <chodolak@gmail.com>
2015-04-26 00:59:07 -0700

    Updated fields.

--- app/src/main/java/com/chodolak/gmailtesting/MainActivity.java
+++ app/src/main/java/com/chodolak/gmailtesting/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.sun.mail.imap.IMAPFolder;
 import com.sun.mail.imap.IMAPStore;
 
+import java.util.ArrayList;
 import java.util.Properties;
 
 import javax.mail.MessagingException;
@@ -18,14 +23,25 @@ import javax.mail.*;
 
 public class MainActivity extends ActionBarActivity {
 
+    ArrayList<String> subjectList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button b = (Button)findViewById(R.id.button);
+
+        subjectList = new ArrayList<String>();
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                subjectList.clear();
+
+                final EditText email = (EditText)findViewById(R.id.editText);
+                final EditText pass = (EditText)findViewById(R.id.passField);
+                System.out.println(email.getText());
+                System.out.println(pass.getText());
+
                 Runnable r = new Runnable() {
                     @Override
                     public void run() {
@@ -37,23 +53,29 @@ public class MainActivity extends ActionBarActivity {
 
                         try {
                             imapStore = (IMAPStore) session.getStore("imaps");
-                            imapStore.connect("imap.gmail.com", "maxzoofake1@gmail.com", "5vevrt12");
+                            imapStore.connect("imap.gmail.com", email.getText().toString(), pass.getText().toString());
                             final IMAPFolder folder = (IMAPFolder) imapStore.getFolder("Inbox");
                             folder.open(IMAPFolder.READ_WRITE);
                             Message m[] = folder.getMessages();
                             for (Message n : m) {
+                                subjectList.add(n.getSubject());
                                 System.out.println(n.getSubject());
+
                             }
                         } catch (MessagingException e) {
                             e.printStackTrace();
                         }
 
                     }
+
                 };
                 Thread t = new Thread(r);
                 t.start();
+                updateList();
+
             }
         });
+
     }
 
     @Override
@@ -77,6 +99,13 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void updateList(){
+        ListView animalList = (ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, subjectList);
+        animalList.setAdapter(arrayAdapter);
+    }
+
 
 }
 
